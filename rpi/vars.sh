4da3d18f738fb6e647ff1ca23dc748cdf7665bd4
Lukas Rausche <lukas@rausche.de>
2023-10-13 18:29:46 +0200

    No need for extra sub directory

--- rpi/vars.sh
+++ rpi/vars.sh
@@ -1,12 +1,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-MKXPZ_PREFIX=rpi
-export LDFLAGS="-L$DIR/build-${MKXPZ_PREFIX}/lib"
-export CFLAGS="-I$DIR/build-${MKXPZ_PREFIX}/include"
+MKXPZ_PREFIX=""
+export LDFLAGS="-L$DIR/build/lib"
+export CFLAGS="-I$DIR/build/include"
 MKXPZ_OLD_PC=$(pkg-config --variable pc_path pkg-config)
 
 # Try to load the stuff we built first
-export PKG_CONFIG_LIBDIR="$DIR/build-${MKXPZ_PREFIX}/lib/pkgconfig:$DIR/build-${MKXPZ_PREFIX}/lib64/pkgconfig:${MKXPZ_OLD_PC}"
-export PATH="$DIR/build-${MKXPZ_PREFIX}/bin:$PATH"
-export LD_LIBRARY_PATH="$DIR/build-${MKXPZ_PREFIX}/lib:${LD_LIBRARY_PATH}"
-export MKXPZ_PREFIX="$DIR/build-${MKXPZ_PREFIX}"
+export PKG_CONFIG_LIBDIR="$DIR/build/lib/pkgconfig:$DIR/build/lib64/pkgconfig:${MKXPZ_OLD_PC}"
+export PATH="$DIR/build/bin:$PATH"
+export LD_LIBRARY_PATH="$DIR/build/lib:${LD_LIBRARY_PATH}"
+export MKXPZ_PREFIX="$DIR/build"
